503680da3b67c0a89b8d15bd5dd022df817a7412
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-03-24 22:11:05 -0300

    :sparkles: feat: lógica para download dos arquivos em pdf implementada

--- web_scraping/scraping.py
+++ web_scraping/scraping.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+import os
+import requests
+
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
@@ -22,6 +25,17 @@ pdf_links = [
             ("Anexo_I" in link.get_attribute("href") or "Anexo_II" in link.get_attribute("href"))
             ]
 
-print(pdf_links)
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+for link in pdf_links:
+    reponse = requests.get(link, stream=True)
+    if reponse.status_code == 200:
+        filename = os.path.join(dir_path, link.split("/")[-1])
+        with open(filename, "wb") as file:
+            for chunk in reponse.iter_content(1024):
+                file.write(chunk)
+        print(f"Arquivo {filename} baixado com sucesso")
+    else:
+        print(f"Erro ao baixar {link}: {reponse.status_code}")   
 
 driver.quit()
