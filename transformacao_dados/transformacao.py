2654b0fac8fad80b43f4c636436ab7116e212dfa
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-03-25 18:20:05 -0300

    :sparkles: feat: lógica de busca por tabelas no pdf implementada

--- transformacao_dados/transformacao.py
+++ transformacao_dados/transformacao.py
@@ -14,3 +14,16 @@ try:
 except ImportError:
     print("Pandas não está instalado. Instalando...")
     subprocess.check_call([sys.executable, "-m", "pip", "install", "pandas"])
+
+
+import pdfplumber
+import os
+import pandas as pd
+
+pdf_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../web_scraping/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf")
+csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "teste.csv")
+
+with pdfplumber.open(pdf_path) as pdf:
+    for i, page in enumerate(pdf.pages):
+        tables = page.extract_tables()
+        print(f"Página {i+1}: {len(tables)} tabelas encontradas")
