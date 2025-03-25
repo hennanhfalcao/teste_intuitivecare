a37502014eca2118dd469f7080f5efdbb6522959
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-03-24 21:56:05 -0300

    :sparkles: feat: extração dos links para download dos pdfs

--- web_scraping/scraping.py
+++ web_scraping/scraping.py
@@ -12,7 +12,16 @@ driver = webdriver.Chrome(service=service, options=options)
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 driver.get(url)
 
-title = driver.find_element(By.TAG_NAME, "h1").text
-print(title)
+div_content = driver.find_element(By.CLASS_NAME, "cover-richtext-tile")
+links = div_content.find_elements(By.TAG_NAME, "a")
+
+pdf_links = [
+            link.get_attribute("href")
+            for link in links
+            if link.get_attribute("href") and link.get_attribute("href").endswith(".pdf") and 
+            ("Anexo_I" in link.get_attribute("href") or "Anexo_II" in link.get_attribute("href"))
+            ]
+
+print(pdf_links)
 
 driver.quit()
