a7efc0fcc4881a14faefe7291a6d9246ce36ab5f
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-03-26 17:42:32 -0300

    :recycle: refactor:  Mudança na biblioteca de extração das tabelas, utilizando tabula-py

--- transformacao_dados/transformacao.py
+++ transformacao_dados/transformacao.py
@@ -1,29 +1,39 @@
-import subprocess
 import sys
-
-try: 
-    import pdfplumber
-    print("PDF Plumber já está instalado")
-except ImportError:
-    print("PDF Plumber não está instalado. Instalando...")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "pdfplumber"])
-
-try:
-    import pandas
-    print("Pandas já está instalado")
-except ImportError:
-    print("Pandas não está instalado. Instalando...")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "pandas"])
-
-
-import pdfplumber
 import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from utils import instalar_pacotes
+
+instalar_pacotes("tabula-py")
+instalar_pacotes("pandas")
+instalar_pacotes("jpype1")
+
+import os
+from tabula import read_pdf
 import pandas as pd
+import zipfile
+
 
 pdf_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../web_scraping/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf")
-csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "teste.csv")
+csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tabela_completa.csv")
+zip_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Teste_Hennan_Heim_Falcao.zip")
 
-with pdfplumber.open(pdf_path) as pdf:
-    for i, page in enumerate(pdf.pages):
-        tables = page.extract_tables()
-        print(f"Página {i+1}: {len(tables)} tabelas encontradas")
+tabelas_extraidas = read_pdf(pdf_path, pages="3-181", multiple_tables=True, lattice=True)
+
+df = pd.DataFrame
+
+df_final = tabelas_extraidas[0]
+
+for i in range(1, len(tabelas_extraidas)):
+    df_final = pd.concat([df_final, tabelas_extraidas[i]], ignore_index=True)
+
+df_final.columns = df_final.columns.str.replace(r'\r', ' ', regex=True)
+df_final = df_final.replace(r'\r',' ', regex=True)
+df_final.columns = df_final.columns.str.replace('OD', 'ODONTOLÓGICO', regex=True)
+df_final.columns = df_final.columns.str.replace('AMB', 'AMBULATÓRIO', regex=True)
+df_final = df_final.where(pd.notna(df_final), None)
+
+df_final.to_csv(csv_path, index=False, sep=";", encoding="utf-8-sig", na_rep="NULL")
+
+with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    zipf.write(csv_path, os.path.basename(csv_path))
