9a03c0bd783052b4d831d0bc64131b5446f255c9
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-03-26 18:02:07 -0300

    :sparkles: feat: lógica de compactação implementada

--- transformacao_dados/transformacao.py
+++ transformacao_dados/transformacao.py
@@ -16,6 +16,7 @@ import zipfile
 
 pdf_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../web_scraping/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf")
 csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tabela_completa.csv")
+zip_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Teste_Hennan_Heim_Falcao.zip")
 
 tabelas_extraidas = read_pdf(pdf_path, pages="3-181", multiple_tables=True, lattice=True)
 
@@ -33,3 +34,6 @@ df_final.columns = df_final.columns.str.replace('AMB', 'AMBULATÓRIO', regex=Tru
 df_final = df_final.where(pd.notna(df_final), None)
 
 df_final.to_csv(csv_path, index=False, sep=";", encoding="utf-8-sig", na_rep="NULL")
+
+with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    zipf.write(csv_path, os.path.basename(csv_path))
