850e8143bf11d948efec7fa577fe22e084c8f266
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-03-27 19:38:13 -0300

    :card_file_box: raw: scripts de estruturação das tabelas no banco de dados postgres

---
+++ banco_de_dados/sql_scripts/tables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE demonstracoes_contabeis (
+    data DATE,
+    reg_ans INT,
+    cd_conta_contabil VARCHAR(100),
+    descricao TEXT,
+    vl_saldo_inicial NUMERIC(15,2),
+    vl_saldo_final NUMERIC(15,2),
+    trimestre SMALLINT,
+    ano SMALLINT
+);
+
+CREATE TABLE operadoras_ativas (
+    reg_ans INT PRIMARY KEY,
+    cnpj VARCHAR(20),
+    razao_social TEXT,
+    nome_fantasia TEXT,
+    modalidade TEXT,
+    logradouro TEXT,
+    numero VARCHAR(10),
+    complemento TEXT,
+    bairro TEXT,
+    cidade TEXT,
+    uf CHAR(2),
+    cep VARCHAR(10),
+    ddd VARCHAR(2),
+    telefone VARCHAR(12),
+    fax VARCHAR(12),
+    endereco_eletronico TEXT,
+    representante TEXT,
+    cargo_representante TEXT,
+    regiao_de_comercializacao INT,
+    data_registro_ans DATE
+);
