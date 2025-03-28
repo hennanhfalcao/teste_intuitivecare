99cdc641cb088d264c5ad8d557867d1a3a2f6358
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-03-28 09:37:48 -0300

    :recycle: refactor: refatoração do código de queries analíticas

--- banco_de_dados/sql_scripts/queries_analiticas.sql
+++ banco_de_dados/sql_scripts/queries_analiticas.sql
@@ -2,7 +2,7 @@ SELECT o.razao_social AS nome_operadora, d.reg_ans, SUM(d.vl_saldo_final) AS tot
 FROM public.demonstracoes_contabeis d
 JOIN public.operadoras_ativas o ON d.reg_ans = o.reg_ans
 WHERE d.cd_conta_contabil = '41131'  
-AND d.data BETWEEN '2024-10-01' AND '2024-12-31'  
+AND (d.trimestre = 4 AND d.ano = 2024)  
 GROUP BY o.razao_social, d.reg_ans  
 ORDER BY total_despesas DESC  
 LIMIT 10;
@@ -11,7 +11,7 @@ SELECT o.razao_social AS nome_operadora, d.reg_ans, SUM(d.vl_saldo_final) AS tot
 FROM public.demonstracoes_contabeis d
 JOIN public.operadoras_ativas o ON d.reg_ans = o.reg_ans
 WHERE d.cd_conta_contabil = '41131'  
-AND d.data BETWEEN '2024-01-01' AND '2024-12-31'  
+AND d.ano = 2024  
 GROUP BY o.razao_social, d.reg_ans  
 ORDER BY total_despesas DESC  
 LIMIT 10;
