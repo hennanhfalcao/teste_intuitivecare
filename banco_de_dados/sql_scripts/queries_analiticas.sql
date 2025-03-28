989ec4f52de4bb3bab943ce131d26bf25d823be2
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-03-28 10:18:21 -0300

    :recycle: refactor: utilizando subqueries para encontrar os códigos referentes a descrição

--- banco_de_dados/sql_scripts/queries_analiticas.sql
+++ banco_de_dados/sql_scripts/queries_analiticas.sql
@@ -1,8 +1,13 @@
 SELECT o.razao_social AS nome_operadora, d.reg_ans, SUM(d.vl_saldo_final) AS total_despesas  
 FROM public.demonstracoes_contabeis d
 JOIN public.operadoras_ativas o ON d.reg_ans = o.reg_ans
-WHERE d.cd_conta_contabil = '41131'  
-AND (d.trimestre = 4 AND d.ano = 2024)  
+WHERE d.cd_conta_contabil IN (
+    SELECT DISTINCT cd_conta_contabil
+    FROM public.demonstracoes_contabeis
+    WHERE descricao LIKE '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS%HOSPITALAR%'
+    )
+AND d.trimestre = 4 
+AND d.ano = 2024  
 GROUP BY o.razao_social, d.reg_ans  
 ORDER BY total_despesas DESC  
 LIMIT 10;
@@ -10,8 +15,12 @@ LIMIT 10;
 SELECT o.razao_social AS nome_operadora, d.reg_ans, SUM(d.vl_saldo_final) AS total_despesas  
 FROM public.demonstracoes_contabeis d
 JOIN public.operadoras_ativas o ON d.reg_ans = o.reg_ans
-WHERE d.cd_conta_contabil = '41131'  
-AND d.ano = 2024  
+WHERE d.cd_conta_contabil IN (
+    SELECT DISTINCT cd_conta_contabil
+    FROM public.demonstracoes_contabeis
+    WHERE descricao LIKE '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS%HOSPITALAR%'
+    )    
+AND d.ano = 2024
 GROUP BY o.razao_social, d.reg_ans  
 ORDER BY total_despesas DESC  
 LIMIT 10;
